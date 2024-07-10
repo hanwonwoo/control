ad4037afeee2685b2c9a874416d0124badd7335f
hanwonwoo <wonwoo3272@naver.com>
2024-07-10 17:08:56 +0900

    true11
    연습문제

---
+++ 연습문제.py
@@ -0,0 +1,19 @@
+#1. 사용자로부터 값을 입력
+#2. 사용자로부터
+#3. 변환된 값을 각 조건에 따라 학점으로 ㅜㄹ력
+
+
+
+num = input("점수를 입력하세요:")
+
+
+num = int(num)
+if 81 <= num <= 100 :
+    print("A")
+if num >= 81 and num <= 100:
+    print("A")
+elif 61<= num <= 80:
+    print("B")
+
+
+
